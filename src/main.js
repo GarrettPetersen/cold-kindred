60d990f1d6b77be60a982989b2c14248aceb2112
GarrettPetersen <garrett.m.petersen@gmail.com>
2025-10-03 16:14:44 -0700

    landing screen

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <main>
-    <h1>Cold Kindred</h1>
-    <p>A procedural cold case investigation.</p>
-    <button id="start">Start</button>
+    <section class="hero">
+      <h1>Cold Kindred</h1>
+      <p class="tagline">A procedural cold case investigation.</p>
+      <button id="start" class="start">Start Investigation</button>
+    </section>
   </main>
 `;
 
 document.getElementById('start').addEventListener('click', () => {
-    alert('Simulation coming soon.');
+  alert('Simulation coming soon.');
 });
 
 
